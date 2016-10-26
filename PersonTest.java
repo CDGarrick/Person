ebdb985aff91ffbfde6589d55a6cf6d58487fa0d
Caleb Garrick <caleb.garrick@mail.lipscomb.edu>
2016-10-26 16:19:08 -0400

    Added Person Files

---
+++ PersonTest.java
@@ -0,0 +1,8 @@
+public class PersonTest{
+  public static void main(String[]args){
+    Person def=new Person();
+    Person custom=new Person("Caleb","Garrick","37204");
+    def.printPerson();
+    custom.printPerson();
+  }
+}
